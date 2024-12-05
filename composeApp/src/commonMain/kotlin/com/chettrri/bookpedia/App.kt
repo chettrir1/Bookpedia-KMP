329a418c0ee8961f7e74f1fe388bfce7a890cf27
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-12-05 15:58:08 -0500

    custom search bar added

--- composeApp/src/commonMain/kotlin/com/chettrri/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/chettrri/bookpedia/App.kt
@@ -1,9 +1,17 @@
 package com.chettrri.bookpedia
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.chettrri.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.chettrri.bookpedia.book.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    BookListScreenRoot(
+        viewModel = remember { BookListViewModel() },
+        onBookCLick = {
+        }
+    )
 }
--- composeApp/src/commonMain/kotlin/com/chettrri/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/chettrri/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -1,8 +1,12 @@
 package com.chettrri.bookpedia.book.presentation.book_list.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
@@ -12,12 +16,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import bookpedia_cmp.composeapp.generated.resources.Res
 import bookpedia_cmp.composeapp.generated.resources.close_hint
 import bookpedia_cmp.composeapp.generated.resources.search_hint
 import com.chettrri.bookpedia.core.presentation.DarkBlue
+import com.chettrri.bookpedia.core.presentation.DesertWhite
 import com.chettrri.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.stringResource
 
@@ -28,45 +37,64 @@ fun BookSearchBar(
     onImeSearch: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = onSearchQueryChange,
-        shape = RoundedCornerShape(100),
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = DarkBlue,
-            focusedLabelColor = SandYellow
-        ),
-        placeholder = {
-            Text(text = stringResource(Res.string.search_hint))
-        },
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
-            )
-        },
-        singleLine = true,
-        keyboardActions = KeyboardActions(
-            onSearch = {
-                onImeSearch()
-            }
-        ),
-        trailingIcon = {
-            AnimatedVisibility(
-                visible = searchQuery.isNotBlank()
-            ) {
-                IconButton(
-                    onClick = {
-                        onSearchQueryChange("")
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Clear,
-                        contentDescription = stringResource(Res.string.close_hint)
-                    )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue,
+                focusedLabelColor = SandYellow
+            ),
+            placeholder = {
+                Text(text = stringResource(Res.string.search_hint))
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                )
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeSearch()
                 }
-            }
-        }
-    )
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Clear,
+                            contentDescription = stringResource(Res.string.close_hint),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+            modifier = modifier
+                .background(
+                    shape = RoundedCornerShape(100),
+                    color = DesertWhite
+                )
+                .minimumInteractiveComponentSize()
+        )
+    }
 }
